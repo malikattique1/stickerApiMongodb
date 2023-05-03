5b4a189b4a0f7984ec0de66ea53161e12d5904e9
Attique ur rehman <119055433+attiqquefunsoltech@users.noreply.github.com>
2023-05-03 17:49:52 +0500

    index.js

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 require('dotenv').config({ path: './config.env' });
 
 var express = require('express');
-var apptwo= express();
+var index= express();
 
-apptwo.use(express.json());
+index.use(express.json());
 const bodyParser = require('body-parser');
-apptwo.use(bodyParser.urlencoded({ extended: true })); 
+index.use(bodyParser.urlencoded({ extended: true })); 
 
 const path = require('path');
-apptwo.use('/upload', express.static(path.join(__dirname, 'upload')))
+index.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 const cors = require('cors');
-apptwo.use(cors({origin: true, credentials: true}));
+index.use(cors({origin: true, credentials: true}));
 
-apptwo.use(function(req, res, next) {
+index.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-apptwo.use(function(req, res, next) {
+index.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
@@ -30,22 +30,22 @@ apptwo.use(function(req, res, next) {
 
 
 global.__root   = __dirname + '/'; 
-apptwo.get('/api', function (req, res) {
+index.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
 
 var UserController = require(__root + 'api/user/UserController');
-apptwo.use('/api/user', UserController);
+index.use('/api/user', UserController);
 
 var StickerController = require(__root + 'api/sticker/StickerController');
-apptwo.use('/api/sticker', StickerController);
+index.use('/api/sticker', StickerController);
 
 var StickerpackController = require(__root + 'api/stickerpack/StickerpackController');
-apptwo.use('/api/stickerpack', StickerpackController);
+index.use('/api/stickerpack', StickerpackController);
 
 const port = process.env.PORT || 3000;
-apptwo.listen(port, () => {
+index.listen(port, () => {
   console.log("server up and running on PORT :", port);
 });
 
-module.exports = apptwo;
+module.exports = index;
