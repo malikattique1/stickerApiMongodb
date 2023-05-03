c57e7a5a1d54336770bcf144e7e3aecd89eeeb39
Attique ur rehman <119055433+attiqquefunsoltech@users.noreply.github.com>
2023-05-03 17:36:30 +0500

    deleted engine added env

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/stickerapp', { useMongoClient: true });
 
-mongoose.connect(`mongodb+srv://attiqueurrehmannew12:5251050@cluster0.ifmxcrl.mongodb.net/Stickerapp?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log("Mongoose Connected");
 }).catch((error) => {
