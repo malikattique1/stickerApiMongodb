b3c2c38581a6393186161f4f17a2d3be5ba7b922
Attique ur rehman <119055433+attiqquefunsoltech@users.noreply.github.com>
2023-05-04 10:48:16 +0500

    vercelup

--- index.js
+++ index.js
@@ -1,61 +1,39 @@
-// require("dotenv").config();
 require('dotenv').config({ path: './config.env' });
-// import mongoose from "mongoose";
-var mongoose = require('mongoose');
-// import "dotenv/config";
-
-
-
-var express = require('express');
-var index= express();
-
-index.use(express.json());
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-index.use(bodyParser.urlencoded({ extended: true })); 
-
 const path = require('path');
-index.use('/upload', express.static(path.join(__dirname, 'upload')))
 
-const cors = require('cors');
-index.use(cors({origin: true, credentials: true}));
+const app = express();
 
-index.use(function(req, res, next) {
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-index.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-
-
-global.__root   = __dirname + '/'; 
-index.get('/api', function (req, res) {
+app.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
 
-var UserController = require(__root + 'api/user/UserController');
-index.use('/api/user', UserController);
+app.use('/api/user', require('./api/user/UserController'));
 
-var StickerController = require(__root + 'api/sticker/StickerController');
-index.use('/api/sticker', StickerController);
-
-var StickerpackController = require(__root + 'api/stickerpack/StickerpackController');
-index.use('/api/stickerpack', StickerpackController);
-
-const port = process.env.PORT
-// index.listen(port, () => {
-//   console.log("server up and running on PORT :", port);
-// });
+const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Mongodb connected");
-  index.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
 }).catch((err) => {
@@ -63,11 +41,4 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   process.exit(1);
 });
 
-
-
-
-
-
-
-
-module.exports = index;
+module.exports = app;
