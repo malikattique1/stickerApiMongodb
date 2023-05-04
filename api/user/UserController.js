431e095b5b760960e6834e03257edb4229b4a96c
Attique ur rehman <119055433+attiqquefunsoltech@users.noreply.github.com>
2023-05-04 14:43:38 +0500

    vercelup

--- api/user/UserController.js
+++ api/user/UserController.js
@@ -43,13 +43,13 @@ router.get("/", async (req, res) => {
 
 
 // GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', VerifyToken, function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
-});
+// router.get('/:id', VerifyToken, function (req, res) {
+//     User.findById(req.params.id, function (err, user) {
+//         if (err) return res.status(500).send("There was a problem finding the user.");
+//         if (!user) return res.status(404).send("No user found.");
+//         res.status(200).send(user);
+//     });
+// });
 
 
 
