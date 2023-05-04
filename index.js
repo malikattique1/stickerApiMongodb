99d1e04e25165df9c1be3cdc1729967d6d5daaf8
Attique ur rehman <119055433+attiqquefunsoltech@users.noreply.github.com>
2023-05-04 13:01:13 +0500

    vercelup

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ index.get('/api', function (req, res) {
 
 // var UserController = require(__root + 'api/user/UserController');
 // const UserController = require("./api/user/UserController");
-// index.use('/api/user/UserController', UserController);
+const UserController = require("./api/UserController");
+index.use('/api/user', UserController);
 
 // var StickerController = require(__root + 'api/sticker/StickerController');
 // index.use('/api/sticker', StickerController);
